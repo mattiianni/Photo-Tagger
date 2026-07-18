e10acd13b6a98bab7ce4e8eb07622a979643ba5e
Mattia Ianniello <mattiaianniello@192.168.1.82>
2026-07-18 12:04:51 +0200

    Change Gemini API version from v1beta to v1 in endpoint URL

--- frontend/download-models.js
+++ frontend/download-models.js
@@ -12,10 +12,12 @@ const BASE_URL = 'https://raw.githubusercontent.com/justadudewhohacks/face-api.j
 const files = [
   'ssd_mobilenetv1_model-weights_manifest.json',
   'ssd_mobilenetv1_model-shard1',
+  'ssd_mobilenetv1_model-shard2',
   'face_landmark_68_model-weights_manifest.json',
   'face_landmark_68_model-shard1',
   'face_recognition_model-weights_manifest.json',
-  'face_recognition_model-shard1'
+  'face_recognition_model-shard1',
+  'face_recognition_model-shard2'
 ];
 
 async function downloadFile(file) {
